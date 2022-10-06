b2bff91351ef0265cf5d8c16ba8e6a1518cb5a05
tasheriaj <tasheriaj@gmail.com>
2022-10-06 19:08:38 -0400

    multiplication

---
+++ src/timesTen/bigTen.java
@@ -0,0 +1,17 @@
+package timesTen;
+
+public class bigTen {
+	
+	public static void main (String[] args) {
+		int num = 7;
+		System.out.println("Let's multiply 7 by numbers 1-10!");
+		for (int a = 1; a <= 10; ++a)
+		{
+			
+			System.out.printf("%d * %d = %d \n", num, a, num * a);
+			
+		}
+				
+	}
+
+}
